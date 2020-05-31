9da112dc1fc840f6652e7af2d5b3b5baf5fe7a4e
Sara Aimée Smiseth <51710585+SaraSmiseth@users.noreply.github.com>
2020-05-31 17:26:17 +0200

    proxy65_address = proxy. .. domain

--- conf.d/04-vhost.cfg.lua
+++ conf.d/04-vhost.cfg.lua
@@ -9,9 +9,8 @@ VirtualHost (domain)
 
 -- Set up a SOCKS5 bytestream proxy for server-proxied file transfers
 Component ("proxy." .. domain) "proxy65"
-	proxy65_address = domain
+	proxy65_address = "proxy." .. domain
 	proxy65_acl = { domain }
-
 
 -- Set up a http file upload because proxy65 is not working in muc
 -- Component (domain) "http_upload"
